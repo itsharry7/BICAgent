4bb71c641b2843d5c29ce4a489b699d64e713856
Hariyansh Sehajpal <52382982+itsharry7@users.noreply.github.com>
2025-09-04 00:44:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ groq_chat = ChatGroq(
     groq_api_key="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",
     model_name="llama-3.1-8b-instant",
     temperature=0.9,
-    max_tokens=200
+    max_tokens=1000
 )
 
 # ---------------- Web Search (DuckDuckGo fallback) ----------------
